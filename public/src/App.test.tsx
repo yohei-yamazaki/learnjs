e4422ed42dc03b3bfc15719003b48c3740836c1b
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2019-12-25 00:25:32 +0900

    red test

--- public/src/App.test.tsx
+++ public/src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { createHashHistory } from 'history';
 import App from './App';
 
 test('render Problem view', () => {
-  const { container, getByText } = render(<App />);
-  fireEvent.click(getByText(/Start Now!/i));
+  const history = createHashHistory();
+  history.push('/problem');
+  const { container } = render(
+    <App />,
+  );
   expect(container.innerHTML).toMatch('Coming soon!');
 });
