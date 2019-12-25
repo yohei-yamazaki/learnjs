e1822e0561d88518d829ff427c33c739ea4c37d0
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2019-12-26 00:06:26 +0900

    App.tsxでハッシュがない時にlandingページが出るテスト

--- public/src/App.test.tsx
+++ public/src/App.test.tsx
@@ -3,6 +3,14 @@ import { render } from '@testing-library/react';
 import { createHashHistory } from 'history';
 import App from './App';
 
+test('render landing page when there is no hash', () => {
+  const history = createHashHistory();
+  render(
+    <App />,
+  );
+  expect(history.location.pathname).toBe('/');
+});
+
 test('render Problem view', () => {
   const history = createHashHistory();
   history.push('/problem');
