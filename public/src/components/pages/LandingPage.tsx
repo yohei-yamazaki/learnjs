4b30cd3973151b71f0a9042bccab94a046aff62a
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2019-12-25 23:53:44 +0900

    landingpageの修正

--- public/src/components/pages/LandingPage.tsx
+++ public/src/components/pages/LandingPage.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Button, Container, Grid, Header } from 'semantic-ui-react';
+import { RouteComponentProps } from 'react-router-dom';
 
-type Props = {};
+type Props = {} & RouteComponentProps;
 
-const LandingPage: React.FC<Props> = () => {
+const LandingPage: React.FC<Props> = (props) => {
+  const { history } = props;
   return (
     <Container>
       <Grid>
         <Grid.Row>
           <Grid.Column width={8} verticalAlign="bottom">
             <Header as="h2">Learn JavaScript, one puzzle at a time.</Header>
-            <Button>Start Now!</Button>
+            <Button onClick={() => { history.push('/problem'); }}>Start Now!</Button>
           </Grid.Column>
           <Grid.Column width={8}>
             <img src="../../../logo512.png" alt="logo" />
