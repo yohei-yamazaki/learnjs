a496356a45a8f576a499dfc2fa88207ebb9e71e9
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2019-12-25 23:20:28 +0900

    App.tsxのroutingの設定

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Switch, Route, HashRouter } from 'react-router-dom';
 import './App.css';
 import LandingPage from './components/pages/LandingPage';
 
-const App: React.FC = () => <LandingPage />;
+const ProblemPage: React.FC = () => <h1>Coming soon!</h1>;
+
+const App: React.FC = () => {
+  return (
+    <HashRouter>
+      <Switch>
+        <Route path="/problem" component={ProblemPage} />
+        <Route extact strict sensitive path="/" component={LandingPage} />
+      </Switch>
+    </HashRouter>
+  );
+};
 
 export default App;
