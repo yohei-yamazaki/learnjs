8ec3e7231d219b89791c7fa872a3d1f7701646b6
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2019-12-27 02:35:30 +0900

    CodeContainerの追加

--- public/src/components/molecules/body/ProblemBody.tsx
+++ public/src/components/molecules/body/ProblemBody.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Header } from 'semantic-ui-react';
+import styled from 'styled-components';
 import { Problem } from '../../pages/ProblemPage';
 
 type Props = {
   problem: Problem
 }
 
+const CodeContainer = styled.div`
+  background-color: lightgrey;
+  padding: 16px;
+`;
+
 const ProblemBody: React.FC<Props> = (props) => {
   const { problem } = props;
   return (
     <div>
       <Header as="h3">{problem.description}</Header>
-      <pre>{problem.code}</pre>
+      <CodeContainer>
+        <pre><code>{problem.code}</code></pre>
+      </CodeContainer>
     </div>
   );
 };
