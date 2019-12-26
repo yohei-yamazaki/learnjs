9ae5742803b85706e3f4a56ccf4dbded2456b210
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2019-12-27 02:06:15 +0900

    mock dataのproblemsを用意

--- public/src/components/pages/ProblemPage.tsx
+++ public/src/components/pages/ProblemPage.tsx
@@ -3,6 +3,19 @@ import { Container, Header } from 'semantic-ui-react';
 import { RouteComponentProps } from 'react-router-dom';
 import ProblemTitle from '../atoms/ProblemTitle';
 
+const problems: {id: string, description: string, code: string}[] = [
+  {
+    id: '1',
+    description: 'What is truth?',
+    code: 'const truthy = () => { return __; }',
+  },
+  {
+    id: '2',
+    description: 'Simple Math',
+    code: 'const simpleMath = () => { return 42 === 6 * __; }',
+  },
+];
+
 type Props = {} & RouteComponentProps<{id: string}>;
 
 const ProblemPage: React.FC<Props> = (props) => {
