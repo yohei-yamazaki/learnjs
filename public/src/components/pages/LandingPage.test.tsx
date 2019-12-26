b0a1d466e840495ddeb44df2f396cd61b772b5e9
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2019-12-27 01:16:43 +0900

    Problem/1にいくテストを追加

--- public/src/components/pages/LandingPage.test.tsx
+++ public/src/components/pages/LandingPage.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import LandingPage from './LandingPage';
+import App from '../../App';
 
 test('renders Start Now! text', () => {
   const { getByText } = render(<LandingPage />);
   const linkElement = getByText(/Start Now!/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Problem1 page', () => {
+  const { getByText, container } = render(<App />);
+  fireEvent.click(getByText(/Start Now!/i));
+  expect(container.innerHTML).toMatch('Problem:1');
 });
