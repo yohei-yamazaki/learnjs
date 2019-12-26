b4aadd07049726662c151963407bc180960b6ffc
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2019-12-27 01:37:24 +0900

    ProblemTitleを適用

--- public/src/components/pages/ProblemPage.tsx
+++ public/src/components/pages/ProblemPage.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Container, Header } from 'semantic-ui-react';
 import { RouteComponentProps } from 'react-router-dom';
+import ProblemTitle from '../atoms/ProblemTitle';
 
 type Props = {} & RouteComponentProps<{id: string}>;
 
 const ProblemPage: React.FC<Props> = (props) => {
   const { match } = props;
   const { id } = match.params;
-  console.log(match.params);
-
   return (
     <Container>
-      <Header as="h1">{`Problem:${id}`}</Header>
+      <ProblemTitle problemNumber={id} />
     </Container>
   );
 };
