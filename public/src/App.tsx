f4318375554a100f5b158c9288af5b6eaf13ad13
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2019-12-28 18:32:53 +0900

    ナビゲーションバーの追加

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Switch, Route, HashRouter } from 'react-router-dom';
+import { Switch, Route, HashRouter, NavLink } from 'react-router-dom';
 import './App.css';
+import { Menu, Dropdown, Header } from 'semantic-ui-react';
 import LandingPage from './components/pages/LandingPage';
 import ProblemPage from './components/pages/ProblemPage';
 
 const App: React.FC = () => {
+  const problemCategories = [
+    { id: '1', title: 'Truthy' },
+    { id: '2', title: 'Caluculate' },
+  ];
+
   return (
     <HashRouter>
+      <Menu borderless>
+        <Menu.Item header><Header as="h1">LearnJS</Header></Menu.Item>
+        <Menu.Item as={NavLink} exact to="/">Landing</Menu.Item>
+        <Dropdown item text="Problems">
+          <Dropdown.Menu>
+            {problemCategories.map((problemCategory) => {
+              return (
+                <Dropdown.Item as={NavLink} key={problemCategory.id} exact to={`/problem/${problemCategory.id}`}>{problemCategory.title}</Dropdown.Item>
+              );
+            })}
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu>
       <Switch>
         <Route path="/problem/:id" component={ProblemPage} />
         <Route extact strict sensitive path="/" component={LandingPage} />
