459f2e2c2760d875866a241fc945d0dcfc663fa2
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2019-12-31 13:19:28 +0900

    login状態の取得

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,37 +1,31 @@
-import React from 'react';
-import { Switch, Route, HashRouter, NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import Amplify, { Auth, Hub } from 'aws-amplify';
 import './App.css';
-import { Menu, Dropdown, Header } from 'semantic-ui-react';
-import LandingPage from './components/pages/LandingPage';
-import ProblemPage from './components/pages/ProblemPage';
+import awsconfig from './aws-exports';
+import Routing from './components/Routing';
+
+Amplify.configure(awsconfig);
 
 const App: React.FC = () => {
-  const problemCategories = [
-    { id: '1', title: 'Truthy' },
-    { id: '2', title: 'Caluculate' },
-  ];
-
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    Hub.listen('auth', ({ payload: { event, data } }) => {
+      switch (event) {
+        case 'signIn':
+          setUser(data);
+          break;
+        default:
+          setUser(null);
+          break;
+      }
+    });
+    Auth.currentAuthenticatedUser()
+      .then((currentUser) => setUser(currentUser))
+      .catch(() => console.log('Not signed in'));
+  }, []);
   return (
-    <HashRouter>
-      <Menu borderless>
-        <Menu.Item header><Header as="h1">LearnJS</Header></Menu.Item>
-        <Menu.Item as={NavLink} exact to="/">Landing</Menu.Item>
-        <Dropdown item text="Problems">
-          <Dropdown.Menu>
-            {problemCategories.map((problemCategory) => {
-              return (
-                <Dropdown.Item as={NavLink} key={problemCategory.id} exact to={`/problem/${problemCategory.id}`}>{problemCategory.title}</Dropdown.Item>
-              );
-            })}
-          </Dropdown.Menu>
-        </Dropdown>
-      </Menu>
-      <Switch>
-        <Route path="/problem/:id" component={ProblemPage} />
-        <Route extact strict sensitive path="/" component={LandingPage} />
-      </Switch>
-    </HashRouter>
+    <Routing user={user} />
   );
 };
 
-export default App;
+export default (App);
