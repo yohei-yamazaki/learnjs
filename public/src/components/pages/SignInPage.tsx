425f1c2b60e6585a946548ab80d93438a520cd34
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2019-12-31 13:21:24 +0900

    loginページの追加

---
+++ public/src/components/pages/SignInPage.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { Container, Input, Button, Form, Loader } from 'semantic-ui-react';
+import { RouteComponentProps } from 'react-router-dom';
+import { useFormik, FormikErrors } from 'formik';
+import { Auth } from 'aws-amplify';
+
+type SignUpValues = {
+  email: string
+  password: string
+}
+
+type Props = {} & RouteComponentProps;
+
+const SignInPage: React.FC<Props> = (props) => {
+  const [loading, setLoading] = useState(false);
+  const { history } = props;
+
+  const formik = useFormik<SignUpValues>({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: async (values) => {
+      const { email, password } = values;
+      setLoading(true);
+      await Auth.signIn({ username: email, password });
+      setLoading(false);
+      history.push('/landing');
+    },
+    validate: (value) => {
+      const errors: FormikErrors<SignUpValues> = {};
+      const { email, password } = value;
+      if (!email) {
+        errors.email = '必須';
+      }
+      if (!password) {
+        errors.password = '必須';
+      }
+      return errors;
+    },
+  });
+
+  return (
+    <>
+      <Container>
+        <Form>
+          <Form.Field label="Email" error={formik.errors.email} value={formik.values.email} control={Input} onChange={formik.handleChange('email') as () => {}} />
+          <Form.Field label="password" error={formik.errors.password} value={formik.values.password} control={Input} type="password" onChange={formik.handleChange('password') as () => {}} />
+          <Form.Field control={Button} onClick={formik.handleSubmit as () => {}}>
+          ログインする
+          </Form.Field>
+        </Form>
+        <Button onClick={() => history.push('/signup')}>新規登録へ</Button>
+      </Container>
+      { loading && <Loader content="Loading" />}
+    </>
+  );
+};
+
+export default SignInPage;
